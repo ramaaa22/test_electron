3a54f71811abeceb7e22860ce9a6ba6f53814c7c
Ramiro Boza <rboza@fnartes.gob.ar>
2020-11-25 11:29:24 -0300

    tres vistas para perfil usuario

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,10 +1,6 @@
 <template>
     <el-main>
-        <!-- <el-row type="flex" justify="center">
-            <el-col class="is-flex is-flex-center text-center">
-                <i class="las la-user-circle fs-12"></i>
-            </el-col>
-        </el-row> -->
+        <!--OPCION 1 -->
         <el-row type="flex" justify="center" class="mt-5">
             <el-col :span="6">
                 <div class="container" >
@@ -63,6 +59,133 @@
                 </el-button>
             </el-col>
         </el-row>
+
+
+
+        <!--OPCION 2 -->
+
+        <el-row type="flex" justify="center" class="mt-5">
+            <el-col :span="6">
+                <div class="container" >
+                    <div align="center">
+                        <el-avatar
+                            shape="circle"
+                            :size="100"
+                            :fit="contain"
+                            :src="user_img">
+                        </el-avatar>
+                    </div>
+                    <div  class="text-center">
+                        <span>{{user.lastname}}, {{user.firstname}}</span>
+                    </div>
+                </div>
+            </el-col>
+        </el-row>
+
+        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
+            <el-col :span="6">
+                <h4 class="text-left">Nombre</h4>
+                <el-card shadow="always" class="non-selectable">
+                    {{user.firstname}}
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
+            <el-col :span="6">
+                <h4 class="text-left">Apellido</h4>
+                <el-card shadow="always" class="non-selectable">
+                    {{user.lastname}}
+                </el-card>
+            </el-col>
+        </el-row>
+
+        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
+            <el-col :span="6">
+                <h4 class="text-left">Email</h4>
+                <el-card shadow="always" class="non-selectable">
+                    {{user.email}}
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
+            <el-col :span="6">
+                <h4 class="text-left">Documento</h4>
+                <el-card shadow="always" class="non-selectable">
+                    {{user.idnumber}}
+                </el-card>
+            </el-col>
+        </el-row>
+
+        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
+            <el-col :span="6">
+                <el-button class="full-width" type="danger" plain>
+                    Cambiar contraseña
+                </el-button>
+            </el-col>
+        </el-row>
+        
+
+
+        <!--OPCION 3 -->
+
+        <el-row type="flex" class="mt-5">
+            <el-col type="flex" :span="6" class="mt-5">
+                <div class="container" >
+                    <div align="center">
+                        <el-avatar
+                            shape="circle"
+                            :size="200"
+                            :fit="contain"
+                            :src="user_img">
+                        </el-avatar>
+                    </div>
+                    <div  class="text-center">
+                        <span>{{user.lastname}}, {{user.firstname}}</span>
+                    </div>
+                </div>
+            </el-col>
+            <el-col type="flex" justify="center" class="mt-5" :span=18>
+                <el-row>
+                    <el-col type="flex" justify="center" class="mt-5" :span=9>
+                        <h4 class="text-left">Nombre</h4>
+                        <el-card shadow="always" class="non-selectable">
+                            {{user.firstname}}
+                        </el-card>
+                    </el-col>
+                    <el-col type="flex" justify="center" class="mt-5" :span=9>
+                        <h4 class="text-left">Apellido</h4>
+                        <el-card shadow="always" class="non-selectable">
+                            {{user.lastname}}
+                        </el-card>
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-col type="flex" justify="center" class="mt-5" :span=9>
+                        <h4 class="text-left">Email</h4>
+                        <el-card shadow="always" class="non-selectable">
+                            {{user.email}}
+                        </el-card>
+                    </el-col>
+                    <el-col type="flex" justify="center" class="mt-5" :span=9>
+                        <h4 class="text-left">Documento</h4>
+                        <el-card shadow="always" class="non-selectable">
+                            {{user.idnumber}}
+                        </el-card>
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-col type="flex" justify="center" class="mt-2" :span=18>
+                        <el-button class="full-width" type="danger" plain>
+                            Cambiar contraseña
+                        </el-button>
+                    </el-col>
+                </el-row>
+            </el-col>
+        </el-row>
+        
+        
+
+
         <!-- <div class="text-right mb-2 pt-2 pr-5 actions">
             <el-tooltip
                 effect="dark"
@@ -148,7 +271,6 @@ import axios from "@/utils/request";
 
 export default {
     mounted(){
-
         let { firstname, lastname, idnumber, email,uuid } = this.$store.getters.user;
         this.user = { firstname, lastname, idnumber, email,uuid };
         this.user_edited = { firstname, lastname, idnumber, email,uuid };
@@ -172,14 +294,14 @@ export default {
                 uuid:'',
             },
             user_data:{},
-            user_img:'https://voxpopulii.in/system/static/dashboard/img/default_user.png',
+            user_img:'https://scontent.faep9-1.fna.fbcdn.net/v/t1.0-9/91175839_10222737247878714_1917986438519980032_o.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=8gtVlg9osswAX-sDWTh&_nc_ht=scontent.faep9-1.fna&oh=ff2a841f2a4654678867eeb1e029ed34&oe=5FE47FFA',
             edit:false,
             label_position:'top'
         }
     ),
     methods:{
-        test(){
-            this.$message('Clic aca')
+        showImage(){
+            console.log('clic en imagen');
         },
         userEdited(){
             if (this.user.idnumber !== this.user_edited.idnumber) {
