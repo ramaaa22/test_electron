3727768ed49a0eb377ad4666d72b57526875c73c
Magalí Defelippe <mdefelippe@fnartes.gob.ar>
2020-11-25 15:12:15 -0300

    Reformas en perfil de usuario

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,267 +1,61 @@
 <template>
-    <el-main>
-        <!--OPCION 1 -->
-        <el-row type="flex" justify="center" class="mt-5">
-            <el-col :span="6">
-                <div class="container" >
-                    <div align="center">
-                        <el-avatar
-                            shape="circle"
-                            :size="100"
-                            :fit="contain"
-                            :src="user_img">
-                        </el-avatar>
-                    </div>
-                    <div  class="text-center">
-                        <span>{{user.lastname}}, {{user.firstname}}</span>
-                    </div>
-                </div>
-            </el-col>
-        </el-row>
-
-        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
-            <el-col :span="6">
-                <h4 class="text-left">Nombre</h4>
-                <el-card shadow="always" class="non-selectable">
-                    {{user.firstname}}
-                </el-card>
+    <el-main class="mt-8">
+        <el-row type="flex" justify="center" class="mt-6">
+            <el-col :span="6" >
+                <el-avatar
+                    shape="circle"
+                    :size="150"
+                    :fit="contain"
+                    :src="user_img">
+                </el-avatar>
             </el-col>
 
-            <el-col :span="6">
-                <h4 class="text-left">Apellido</h4>
-                <el-card shadow="always" class="non-selectable">
-                    {{user.lastname}}
-                </el-card>
-            </el-col>
-        </el-row>
+            <el-col :span="16">
 
-        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
-            <el-col :span="6">
-                <h4 class="text-left">Email</h4>
-                <el-card shadow="always" class="non-selectable">
-                    {{user.email}}
-                </el-card>
-            </el-col>
-
-            <el-col :span="6">
-                <h4 class="text-left">Documento</h4>
-                <el-card shadow="always" class="non-selectable">
-                    {{user.idnumber}}
-                </el-card>
-            </el-col>
-
-        </el-row>
-
-        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
-            <el-col :span="12">
-                <el-button class="full-width" type="danger" plain>
-                    Cambiar contraseña
-                </el-button>
-            </el-col>
-        </el-row>
-
-
-
-        <!--OPCION 2 -->
-
-        <el-row type="flex" justify="center" class="mt-5">
-            <el-col :span="6">
-                <div class="container" >
-                    <div align="center">
-                        <el-avatar
-                            shape="circle"
-                            :size="100"
-                            :fit="contain"
-                            :src="user_img">
-                        </el-avatar>
-                    </div>
-                    <div  class="text-center">
-                        <span>{{user.lastname}}, {{user.firstname}}</span>
-                    </div>
-                </div>
-            </el-col>
-        </el-row>
-
-        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
-            <el-col :span="6">
-                <h4 class="text-left">Nombre</h4>
-                <el-card shadow="always" class="non-selectable">
-                    {{user.firstname}}
-                </el-card>
-            </el-col>
-        </el-row>
-        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
-            <el-col :span="6">
-                <h4 class="text-left">Apellido</h4>
-                <el-card shadow="always" class="non-selectable">
-                    {{user.lastname}}
-                </el-card>
-            </el-col>
-        </el-row>
-
-        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
-            <el-col :span="6">
-                <h4 class="text-left">Email</h4>
-                <el-card shadow="always" class="non-selectable">
-                    {{user.email}}
-                </el-card>
-            </el-col>
-        </el-row>
-        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
-            <el-col :span="6">
-                <h4 class="text-left">Documento</h4>
-                <el-card shadow="always" class="non-selectable">
-                    {{user.idnumber}}
-                </el-card>
-            </el-col>
-        </el-row>
-
-        <el-row type="flex" justify="center" :gutter="20" class="mt-5">
-            <el-col :span="6">
-                <el-button class="full-width" type="danger" plain>
-                    Cambiar contraseña
-                </el-button>
-            </el-col>
-        </el-row>
-        
-
-
-        <!--OPCION 3 -->
-
-        <el-row type="flex" class="mt-5">
-            <el-col type="flex" :span="6" class="mt-5">
-                <div class="container" >
-                    <div align="center">
-                        <el-avatar
-                            shape="circle"
-                            :size="200"
-                            :fit="contain"
-                            :src="user_img">
-                        </el-avatar>
-                    </div>
-                    <div  class="text-center">
-                        <span>{{user.lastname}}, {{user.firstname}}</span>
-                    </div>
-                </div>
-            </el-col>
-            <el-col type="flex" justify="center" class="mt-5" :span=18>
-                <el-row>
-                    <el-col type="flex" justify="center" class="mt-5" :span=9>
-                        <h4 class="text-left">Nombre</h4>
+                <el-row :gutter="20" >
+                    <el-col class="mb-4" :span="12">
+                        <h4 class="text-left">
+                            Nombre
+                        </h4>
                         <el-card shadow="always" class="non-selectable">
                             {{user.firstname}}
                         </el-card>
                     </el-col>
-                    <el-col type="flex" justify="center" class="mt-5" :span=9>
-                        <h4 class="text-left">Apellido</h4>
+
+                    <el-col class="mb-4" :span="12">
+                        <h4 class="text-left">
+                            Apellido
+                        </h4>
                         <el-card shadow="always" class="non-selectable">
                             {{user.lastname}}
                         </el-card>
                     </el-col>
-                </el-row>
-                <el-row>
-                    <el-col type="flex" justify="center" class="mt-5" :span=9>
-                        <h4 class="text-left">Email</h4>
+              
+                    <el-col :span="12">
+                        <h4 class="text-left">
+                            Email
+                        </h4>
                         <el-card shadow="always" class="non-selectable">
                             {{user.email}}
                         </el-card>
                     </el-col>
-                    <el-col type="flex" justify="center" class="mt-5" :span=9>
-                        <h4 class="text-left">Documento</h4>
+                    <el-col :span="12">
+                        <h4 class="text-left">
+                            Documento
+                        </h4>
                         <el-card shadow="always" class="non-selectable">
                             {{user.idnumber}}
                         </el-card>
                     </el-col>
                 </el-row>
-                <el-row>
-                    <el-col type="flex" justify="center" class="mt-2" :span=18>
-                        <el-button class="full-width" type="danger" plain>
-                            Cambiar contraseña
-                        </el-button>
-                    </el-col>
-                </el-row>
+              
+                <el-button class="full-width mt-3" type="danger" plain>
+                    Cambiar contraseña
+                </el-button>
+            
             </el-col>
         </el-row>
-        
-        
 
-
-        <!-- <div class="text-right mb-2 pt-2 pr-5 actions">
-            <el-tooltip
-                effect="dark"
-                content="Editar"
-                v-if="!edit"
-                placement="top">
-                <el-button
-                    size="mini"
-                    type="success"
-                    @click="edit = !edit"
-                    icon="las la-edit"
-                    plain
-                    circle>
-                </el-button>
-            </el-tooltip>
-
-            <el-tooltip
-                v-else
-                effect="dark"
-                content="Guardar cambios"
-                placement="top">
-                <el-button
-                    size="mini"
-                    type="success"
-                    @click="editUserInformation"
-                    icon="las la-save"
-                    plain
-                    circle>
-                </el-button>
-            </el-tooltip>
-
-            <el-tooltip
-                v-if="edit"
-                effect="dark"
-                content="Cancelar"
-                placement="top">
-                <el-button
-                    size="mini"
-                    type="danger"
-                    @click.native="edit=!edit"
-                    icon="las la-window-close"
-                    plain
-                    circle>
-                </el-button>
-            </el-tooltip>
-
-        </div> -->
-
-
-
-
-        <!-- <el-form :label-position="label_position" label-width="100px">
-            <el-form-item label="Nombre">
-                <el-input
-                    :disabled=!edit
-                    v-model="user.firstname">
-                </el-input>
-            </el-form-item>
-            <el-form-item label="Apellido">
-                <el-input
-                    v-model="user.lastname"
-                    :disabled=!edit>
-                </el-input>
-            </el-form-item>
-            <el-form-item label="E-mail">
-                <el-input
-                    v-model="user.email"
-                    :disabled=!edit></el-input>
-            </el-form-item>
-            <el-form-item label="Documento">
-                <el-input
-                    v-model="user.idnumber"
-                    :disabled=!edit>
-                </el-input>
-            </el-form-item>
-        </el-form> -->
     </el-main>
 </template>
 
